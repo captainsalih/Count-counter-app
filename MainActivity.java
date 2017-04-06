d3150b4828fa2f1d5d5df343dddc68f22f5dffb7
captainsalih <captainmozak@gmail.com>
2017-04-06 17:04:51 +0200

    Add files via upload

--- MainActivity.java
+++ MainActivity.java
@@ -1,83 +1,110 @@
-package com.example.android.taranoate;
+package com.example.android.courtcounter_project;
 
-import android.content.Intent;
-import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.view.Menu;
-import android.view.MenuInflater;
-import android.view.MenuItem;
 import android.view.View;
-import android.view.animation.AnimationUtils;
-import android.widget.Button;
-import android.widget.Toast;
-import android.widget.ViewFlipper;
 import android.view.animation.Animation;
-
-import static android.R.attr.id;
-import static android.R.attr.showText;
+import android.view.animation.AnimationUtils;
+import android.widget.TextView;
+import android.widget.ViewFlipper;
 
 public class MainActivity extends AppCompatActivity {
+
+    /**ViewFlipper**/
+
     ViewFlipper viewFlipper;
     Animation fadeIn, fadeOut;
 
+    /** track for team A **/
+    int scoreTeamA = 0 ;
+    /** track for team B **/
+    int scoreTeamB = 0;
+
+
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
+     protected void onCreate(Bundle savedInstanceState) {
+          super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        viewFlipper = (ViewFlipper) findViewById(R.id.viewFlipper);
+         viewFlipper = (ViewFlipper) findViewById(R.id.viewFlipper);
         fadeIn = AnimationUtils.loadAnimation(this, R.anim.fade_in);
         fadeOut = AnimationUtils.loadAnimation(this, R.anim.fade_out);
         viewFlipper.setAutoStart(true);
         viewFlipper.setInAnimation(fadeIn);
         viewFlipper.setOutAnimation(fadeOut);
-        viewFlipper.setFlipInterval(5000);
+        viewFlipper.setFlipInterval(2000);
         viewFlipper.startFlipping();
 
-        //this create the logo
+    }
+    /**
+     * Displays the given score for Team A.
+     */
+    public void displayForTeamA(int score) {
+        TextView scoreView = (TextView) findViewById(R.id.team_a_score);
+        scoreView.setText(String.valueOf(score));
+    }
+    /**
+     * Increase the score for Team A by 1 point.
+     */
+    public void addOneForTeamA(View v) {
+        scoreTeamA = scoreTeamA + 1;
+        displayForTeamA(scoreTeamA);
+    }
 
-       // android.support.v7.app.ActionBar actionBar = getSupportActionBar();
-       // actionBar.setLogo(R.drawable.tara_no_ate11);
-       // actionBar.setDisplayUseLogoEnabled(true);
-      //  actionBar.setDisplayShowHomeEnabled(true);
+    /**
+     * Increase the score for Team A by 2 points.
+     */
+    public void addTwoForTeamA(View v) {
+        scoreTeamA = scoreTeamA + 2;
+        displayForTeamA(scoreTeamA);
+    }
+
+    /**
+     * Increase the score for Team A by 3 points.
+     */
+    public void addThreeForTeamA(View v) {
+        scoreTeamA = scoreTeamA + 3;
+        displayForTeamA(scoreTeamA);
+    }
+
+    /**
+     * Displays the given score for Team B.
+     */
+    public void displayForTeamB(int score) {
+        TextView scoreView = (TextView) findViewById(R.id.team_b_score);
+        scoreView.setText(String.valueOf(score));
+    }
+
+    /**
+     * Increase the score for Team B by 1 point.
+     */
+    public void addOneForTeamB(View v) {
+        scoreTeamB = scoreTeamB + 1;
+        displayForTeamB(scoreTeamB);
+    }
+
+    /**
+     * Increase the score for Team B by 2 points.
+     */
+    public void addTwoForTeamB(View v) {
+        scoreTeamB = scoreTeamB + 2;
+        displayForTeamB(scoreTeamB);
+    }
+
+    /**
+     * Increase the score for Team B by 3 points.
+     */
+    public void addThreeForTeamB(View v) {
+        scoreTeamB = scoreTeamB + 3;
+        displayForTeamB(scoreTeamB);
+    }
+
+    /**RESET**/
+    public void setReset(View v){
+        scoreTeamA = 0;
+        scoreTeamB = 0 ;
+        displayForTeamA(scoreTeamA);
+        displayForTeamB(scoreTeamB);
 
     }
 
-    //custom menu bar
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        MenuInflater inflater = getMenuInflater();
-        inflater.inflate(R.menu.menu, menu);
-        return true;
-    }
-
-    //when bar menu was clicked
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-
-        switch (item.getItemId()) {
-
-            case R.id.about_us:
-
-                Intent intent = new Intent(this, AboutActivity.class);
-                this.startActivity ( intent );
-                return true;
-
-            case  R.id.contact:
-                Intent intent1 = new Intent(this, ContactActivity.class);
-                this.startActivity( intent1);
-                return true;
-
-            case R.id.reservation:
-                Intent intent2 = new Intent(this,ReservationActivity.class);
-                this.startActivity(intent2);
-                return true;
-
-            default:
-
-                return super.onOptionsItemSelected(item);
-        }
-    }
 }
-
-
